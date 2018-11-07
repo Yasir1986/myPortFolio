75284b627f966d2bde732a53e50842c6411e0384
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-11-07 17:15:56 +0200

    contact page updated

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Cell, ListItem, ListItemContent, List } from "react-mdl";
 
 class Contact extends Component {
-    render () {
-        return (
-            <div>
-                <h1>Contact Page</h1>
-            </div>
-        )
-    }
-}
+  render() {
+    return (
+      <div>
+        <div className="contact-body">
+          <Grid className="contact-grid">
+            <Cell col={6}>
+              <h2>Yasir Akbar</h2>
+              <img
+                src="https://openclipart.org/image/2400px/svg_to_png/277081/Male-Avatar.png"
+                alt="avatar"
+                style={{ height: "250px" }}
+              />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+            </Cell>
 
+            <Cell col={6}>
+              <h2>Contact Me</h2>
+              <hr />
+
+            <div className="contact-list">
+            <List>
+                <ListItem>
+                  <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+              
+            </Cell>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Contact;
