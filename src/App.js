79f52f2fe75cd756a1a4278255eb0e3fa3b52b9e
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-11-08 17:45:41 +0200

    Resume Page version 1.0

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title="MyPortFolio" scroll>
+        <Header className="header-color" title={<Link to="/">MyPortFolio</Link>} scroll>
             <Navigation>
                 <Link to ="/"></Link>
                 <Link to ="/resume">Resume</Link>
--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
 
 class Resume extends Component {
     render () {
@@ -16,7 +19,7 @@ class Resume extends Component {
                     </div>
 
                     <h2 style={{paddingTop: '2rem'}}>Yasir Akbar</h2>
-                    <h4 style={{color: 'grey'}}>Programmer</h4>
+                    <h4 style={{color: 'grey'}}>Front End Developer</h4>
                     <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
                     <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
                     <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
@@ -30,7 +33,89 @@ class Resume extends Component {
                     <p>www.yasirakbar.com</p>
 
                     </Cell>
-                    <Cell className="resume-right-col" col={8}Right Side></Cell>
+                    <Cell className="resume-right-col" col={8}>
+                    <h2>Education</h2>
+
+                    <Education 
+                    startYear={2018}
+                    endYear={2018}
+                    schoolName="Integrify"
+                    Degree="Front End Developer Crash Course"
+                    />
+
+                     <Education 
+                    startYear={2017}
+                    endYear={2017}
+                    schoolName="Udemey"
+                    Degree="Full Stack Developer Course"
+                    />
+
+                      <Education 
+                    startYear={2016}
+                    endYear={2016}
+                    schoolName="Coursea"
+                    Degree="Front End Developer Course"
+                    />
+
+
+                     <Education 
+                    startYear={2008}
+                    endYear={2012}
+                    schoolName="Kajaani University of Applied Sciences"
+                    Degree="International Business in BBA"
+                    />
+
+                    <hr style={{borderTop: '3px solid #e22947'}}/>
+
+                    <h2>Experience</h2>
+
+                    <Experience 
+                    startYear={2018}
+                    endYear={2018}
+                    jobName="Front End Developer at Integrify"
+                    jobDescription="Real Life assement project for Integrify Using React/Redux and Nodejs/Express"
+                    />
+
+                    <Experience 
+                    startYear={2016}
+                    endYear={2018}
+                    jobName="Freelancer.com"
+                    jobDescription="Several freelancing project on Front End using HTML/CSS, Wordpress, Jquey, Bootstrap and JavaScript"
+                    />
+
+                    <Experience 
+                    startYear={2015}
+                    endYear={2018}
+                    jobName="Founder at Myphone.fi"
+                    jobDescription="Develop and maintain fully e-commerce website with online Finnish Banking payment system and Paypal"
+                    />
+
+                    
+                    <hr style={{borderTop: '3px solid #e22947'}}/>
+
+                    <h2>Skills</h2>
+
+                    <Skills 
+                        skill="HTML/CSS"    
+                        progress={100}           
+                    />
+
+                     <Skills 
+                        skill="JavaScript"    
+                        progress={75}           
+                    />
+
+                     <Skills 
+                        skill="React/Redux"    
+                        progress={80}           
+                    />
+
+                     <Skills 
+                        skill="Nodejs/Express"    
+                        progress={45}           
+                    />
+
+                    </Cell>
                 </Grid>
             </div>
         )
